53b60a6de9b1fb5a381a25acde8d545052576359
Ivan Szebenszki <szib77@gmail.com>
2019-05-13 09:16:09 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your solution here!
+
+const template = ['Milo', 'Otis', 'Garfield'];
+
+const append = template.slice();
+const prepend = template.slice();
+const removeLast = template.slice();
+const removeFirst = template.slice();
+
+append.push('Odie');
+prepend.unshift('Odie');
+removeLast.pop();
+removeFirst.shift();
